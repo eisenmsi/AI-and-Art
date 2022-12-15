19898cfa14a45f6df225bb5f6f48feaba9e14014
Simon Eisenmann <simon.eisenmann98@web.de>
2022-12-15 16:28:20 +0100

    Add more prompts

--- Stable Diffusion/Different schedulers.py
+++ Stable Diffusion/Different schedulers.py
@@ -12,7 +12,9 @@ pipe.scheduler = LMSDiscreteScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
 
 # Generate image
-prompt = "a photograph of an astronaut riding a horse"
+# prompt = ["a photograph of an astronaut riding a horse"]
+# prompt = ["a photo of medieval knight crying in the rain"]
+prompt = ["Disneyland painted by Van Gogh"]
 
 start = time.process_time()
 start_2 = time.time()
