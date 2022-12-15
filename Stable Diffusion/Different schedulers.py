d140002ab8cdc88273684526f6a397f1f83d68ca
Simon Eisenmann <simon.eisenmann98@web.de>
2022-12-15 16:15:45 +0100

    Create Different schedulers.py

---
+++ Stable Diffusion/Different schedulers.py
@@ -0,0 +1,10 @@
+import torch
+from diffusers import StableDiffusionPipeline
+
+pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+prompt = "a photograph of an astronaut riding a horse"
+image = pipe(prompt).images[0]
+
+image.show()
